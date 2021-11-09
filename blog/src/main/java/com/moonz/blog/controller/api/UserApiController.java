47baa61da6b06fb5b11fa81baa3fba6028d0780d
moonz <banan99@naver.com>
2021-11-09 23:51:21 +0900

    StudyNote 추가 및 Ajax 이용한 회원가입 구현중

--- blog/src/main/java/com/moonz/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/moonz/blog/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.moonz.blog.controller.api;
 
 import com.moonz.blog.dto.ResponseDto;
+import com.moonz.blog.model.RoleType;
 import com.moonz.blog.model.User;
+import com.moonz.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+    @Autowired      // DI
+    private UserService userService;
+
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("회원가입이 호출됨");
 //        return new ResponseDto<Integer>(200, 1);
         // 실제로 DB에 insert하고 아래에서 return.
-        return new ResponseDto<Integer>(HttpStatus.OK, 1);
+        user.setRole(RoleType.USER);    // 추가로 강제로 세팅해줘야하는 필드.
+        int result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK, result);
     }
 }
--- blog/src/main/java/com/moonz/blog/service/UserService.java
+++ blog/src/main/java/com/moonz/blog/service/UserService.java
@@ -4,20 +4,24 @@ import com.moonz.blog.model.User;
 import com.moonz.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
 // 스프링이 컴포넌트 스캔을 통해서 Bean에 등록해줌. IoC(의존성 역전. 메모리에 대신 띄워준다)
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public Long 회원가입(User user) {
+    @Transactional      // 전체에서 실패하면 Rollback됨. 따로 로직을 짜줘야함. 현재는 1개의 트랜잭션이므로 생략.
+    public int 회원가입(User user) {
         try {
             User user1 = userRepository.save(user);
-            return user1.getId();
+            return 1;
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("UserService : 회원가입() : " + e.getMessage());
         }
-        return -1L; //  에러발생하면 여기로 !
+        return -1; //  에러발생하면 여기로 !
     }
 }
