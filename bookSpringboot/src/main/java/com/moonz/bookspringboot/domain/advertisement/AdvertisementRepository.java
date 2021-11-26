8db34ca699190eb3e0b8e68177f14a3e0d319d62
moonz <banan99@naver.com>
2021-11-26 21:42:51 +0900

    디렉토리 정리

--- bookSpringboot/src/main/java/com/moonz/bookspringboot/domain/advertisement/AdvertisementRepository.java
+++
@@ -1,6 +0,0 @@
-package com.moonz.bookspringboot.domain.advertisement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
-}
