165bed7c57860ec684120202afd2253d849a6082
moonz <banan99@naver.com>
2021-11-29 23:57:48 +0900

    WEB MVC 구현 중-홈,조회,등록

--- first-spring/src/main/java/moon/firstspring/SpringConfig.java
+++ first-spring/src/main/java/moon/firstspring/SpringConfig.java
@@ -9,14 +9,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepostiroy()); // memberService는 memberRepository가 필요!
-        // 아래에서 등록된 스프링 빈을 여기에 넣어준다.
-    }
-
-    @Bean
-    public MemberRepository memberRepostiroy() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository()); // memberService는 memberRepository가 필요!
+//        // 아래에서 등록된 스프링 빈을 여기에 넣어준다.
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
--- first-spring/src/main/java/moon/firstspring/controller/MemberController.java
+++ first-spring/src/main/java/moon/firstspring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package moon.firstspring.controller;
 
+import moon.firstspring.domain.Member;
 import moon.firstspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // Spring이 시작될 때 Spring Container가 생성이 되는데 이 @Controller를 보고 해당 MemberController 객체(Bean)를 생성해서 Spring Container에 저장하고 관리한다.
 // 이를 Spring Bean이 관리된다고 함.
@@ -16,6 +19,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";    // 회원가입 완료했으니 홈으로 리다이렉트할때는 이렇게 return
+    }
 
 }
