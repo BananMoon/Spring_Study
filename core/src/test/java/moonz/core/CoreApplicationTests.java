de165fb6f23df2cedf02d860b725b86f937223a9
moonz <banan99@naver.com>
2021-12-07 23:29:42 +0900

    Spring core기능을 java로 구현 project setting

---
+++ core/src/test/java/moonz/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package moonz.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
