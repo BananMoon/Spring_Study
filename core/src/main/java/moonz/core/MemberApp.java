76723a067bc237bfbd0291b87fbb74a5d8976d21
moonz <banan99@naver.com>
2021-12-11 23:34:41 +0900

    스프링으로 전환 및 스프링 컨테이너 사용하기

--- core/src/main/java/moonz/core/MemberApp.java
+++ core/src/main/java/moonz/core/MemberApp.java
@@ -3,12 +3,20 @@ package moonz.core;
 import moonz.core.member.Grade;
 import moonz.core.member.Member;
 import moonz.core.member.MemberService;
+import moonz.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         //   수정
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 스프링 : @Bean을 모두 관리하는 스프링 컨테이너 를 생성!
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);    // AppConfig 파일에 있는 환경 설정정보를 이용해서 Spring Container에 객체를 넣어서 관리하도록 한다.
+        // 스프링 :  직접 찾아오는게아닌 스프링 컨테이너를 통해서 가져온다. @Bean 등록한 메서드 이름과 타입으로 찾아 꺼낸다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);   // Ctrl+ alt + V : 변수 생성
         memberService.join(member);
--- core/src/main/java/moonz/core/OrderApp.java
+++ core/src/main/java/moonz/core/OrderApp.java
@@ -7,17 +7,25 @@ import moonz.core.member.MemberServiceImpl;
 import moonz.core.order.Order;
 import moonz.core.order.OrderService;
 import moonz.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 //TEST (수동)
 public class OrderApp {
     public static void main(String[] args) {
         // 수정
-        MemberService memberService;
-        OrderService orderService;
+//        MemberService memberService;
+//        OrderService orderService;
+//
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
+//        orderService = appConfig.orderService();
 
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-        orderService = appConfig.orderService();
-
+        // 스프링 : 스프링컨테이너 생성
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 스프링 : 스프링 컨테이너에서 객체를 찾아서 꺼내온다. (등록된 메서드명, 꺼낼 클래스 타입)
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
         // 멤버 생성
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
