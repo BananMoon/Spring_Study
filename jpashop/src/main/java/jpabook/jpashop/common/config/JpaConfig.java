0cddf6491b996601214c73211ac1ba67415835af
banan99 <banan99@naver.com>
2022-10-20 23:32:59 +0900

    feat: 필요없는 애노테이션 제거

--- jpashop/src/main/java/jpabook/jpashop/common/config/JpaConfig.java
+++ jpashop/src/main/java/jpabook/jpashop/common/config/JpaConfig.java
@@ -4,12 +4,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityManager;
 
 @Configuration
-@EnableJpaAuditing
 @AllArgsConstructor
 public class JpaConfig {
     private final EntityManager em;
