4c3e7d8be5dca61275582ae3ace7f77357d0e165
BananMoon <younji1115@gmail.com>
2022-11-13 22:27:30 +0900

    style : 주석 수정

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import javax.persistence.Embeddable;
 
 // JPA의 내장타입
-// 값 타입은 한번 정해지면 변경이 불가능해야한다.
+// 값 타입(Value Object)은 한번 정해지면 변경이 불가능해야한다.  => API에 노출해도 괜찮은 클래스
 // JPA 스펙 상 Entity(@Entity)나 Embedded(@Embeddable) 타입은 기본 생성자가 있어야 한다.
 // 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 기술을 사용할 수 있도록 가능하게 해야하기 때문.
 @Embeddable
