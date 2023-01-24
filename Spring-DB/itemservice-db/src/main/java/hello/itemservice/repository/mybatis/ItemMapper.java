a57557ce6e4230ea5b5d55535e0f577cf8a9a3c3
BananMoon <younji1115@gmail.com>
2023-01-24 22:52:12 +0900

    style : 오타수정 및 주석추가

--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -12,11 +12,11 @@ import java.util.Optional;
 /**
  * 마이바이트 매핑 XML을 호출하는 매퍼 인터페이스
  */
-@Mapper // 마이바티스에서 인식!
+@Mapper // 마이바티스에서 인식하여 구현체를 자동으로 만들어 낸다. 구현체는 xml과 연결하는 로직이 아닐까?
 public interface ItemMapper {
 
     void save(Item item);
-    void update(@Param("id") Long id, @Param("updatedParam")ItemUpdateDto updateDto);   // 파라미터 2개 이상인 경우 @Param 필요
+    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateDto);   // 파라미터 2개 이상인 경우 @Param 필요
     List<Item> findAll(ItemSearchCond itemSearch);
     Optional<Item> findById(Long id);
 }
