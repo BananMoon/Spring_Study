6199d8c8d0455aca5215007e334690075303fadf
BananMoon <younji1115@gmail.com>
2023-02-16 16:50:01 +0900

    feat: QueryDsl 이용 v2 - 실용적인 구조(레파지토리 분리) - 단순 쿼리문과 복잡한 쿼리문(querydsl사용) 동작 레파지토리 분리 - config 클래스, 의존하는 service 클래스 추가

--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -20,7 +20,8 @@ import javax.sql.DataSource;
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
 //@Import(SpringDataJpaConfig.class)
-@Import(QuerydslConfig.class)	// 설정파일 import
+//@Import(QuerydslConfig.class)
+@Import(V2Config.class)	// 설정파일 import
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/config/V2Config.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/config/V2Config.java
@@ -1,2 +1,35 @@
-package hello.itemservice.config;public class V2Config {
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+@Configuration
+@RequiredArgsConstructor
+public class V2Config {
+    private final EntityManager em;
+    private final ItemRepositoryV2 itemRepositoryV2;  // SpringDataJpa는 Spring이 자동 등록해주므로 선언만 해놓으면 된다.
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV2(itemRepositoryV2, itemQueryRepositoryV2());
+    }
+
+    @Bean ItemQueryRepositoryV2 itemQueryRepositoryV2() {
+        return new ItemQueryRepositoryV2(em);
+    }
+
+    /* 추가 - TestDataInit 클래스에서 초기 데이터 생성 시 ItemRepository를 의존하기 때문에 빈으로 등록은 되어있어야 한다. */
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV3(em);
+    }
+
 }
