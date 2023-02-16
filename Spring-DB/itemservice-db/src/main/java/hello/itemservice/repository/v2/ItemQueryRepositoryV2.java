111b102beb2a5d0d175d655d6887152183a413e6
BananMoon <younji1115@gmail.com>
2023-02-16 16:50:29 +0900

    feat: QueryDsl 이용 v2 - 실용적인 구조(레파지토리 분리) - 단순 쿼리문과 복잡한 쿼리문(querydsl사용) 동작 레파지토리 분리 - 두 개의 repository 클래스 추가

--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
@@ -1,2 +1,49 @@
-package hello.itemservice.repository.v2;public class ItemQueryRepositoryV2 {
+package hello.itemservice.repository.v2;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.QItem;
+import hello.itemservice.repository.ItemSearchCond;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static hello.itemservice.domain.QItem.*;
+
+/**
+ * 복잡한 쿼리 문제를 해결하는 레파지토리 (실용적인 구조 V2)
+ */
+public class ItemQueryRepositoryV2 {
+    private final JPAQueryFactory query;
+
+
+    public ItemQueryRepositoryV2(EntityManager em) {
+        this.query = new JPAQueryFactory(em);
+    }
+
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+        return query.select(QItem.item)
+                .from(QItem.item)
+                .where(likeItemName(itemName), maxPrice(maxPrice))  // A and B
+                .fetch();
+
+    }
+    // Predicate의 구현체
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
 }
--- Spring-DB/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
+++ Spring-DB/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
@@ -1,2 +1,10 @@
-package hello.itemservice.repository.v2;public interface ItemRepositoryV2 {
+package hello.itemservice.repository.v2;
+
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 스프링데이터JPA 사용 레파지토리
+ */
+public interface ItemRepositoryV2 extends JpaRepository<Item, Long> {
 }
