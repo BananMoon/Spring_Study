b6d872cbdeaa282dfad1ab65f24bad23b77ec661
moonz <banan99@naver.com>
2023-05-06 23:37:00 +0900

    Delete myShopSelect directory

--- myShopSelect/src/main/java/com/moonz/myShopSelect/models/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.moonz.myShopSelect.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
