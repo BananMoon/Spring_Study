e27ba5139f56349a6c38e5a6bdf2f4b21100c05a
BananMoon <younji1115@gmail.com>
2023-05-09 21:41:12 +0900

    feat: 예외 상황에 따른 트랜잭션 롤백과 커밋

--- Spring-DB/springtx/src/test/java/moonz/springtx/exception/RollbackTest.java
+++ Spring-DB/springtx/src/test/java/moonz/springtx/exception/RollbackTest.java
@@ -1,11 +1,78 @@
 package moonz.springtx.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 예외가 터졌는데 왜 커밋하지? 에 초점!
+ * 예외는 비즈니스 예외와 시스템 예외가 있다.
+ * 비즈니스 예외는 데이터가 커밋되어야하는 경우가 있다.
+ */
 @SpringBootTest
 public class RollbackTest {
+    @Autowired
+    RollbackService service;
+    // 출력 : Rolling back JPA transaction on EntityManager
+    @Test
+    @DisplayName("unchecked 예외니까 롤백됐는지 확인")
+    void uncheckedException() {
+        Assertions.assertThatThrownBy(() -> service.uncheckedException())
+                .isInstanceOf(RuntimeException.class);
+    }
+    // 출력 : Committing JPA transaction on EntityManager
+    @Test
+    @DisplayName("checked 예외니까 커밋됐는지 확인")
+    void checkedException() {
+        Assertions.assertThatThrownBy(() -> service.checkedException())
+                .isInstanceOf(MyException.class);
+    }
 
+    // 출력 : Rolling back JPA transaction on EntityManager
+    @Test
+    @DisplayName("rollbackFor 옵션 테스트. checked 예외인데 롤백됐는지 확인")
+    void rollbackfor() {
+        Assertions.assertThatThrownBy(() -> service.rollbackfor())
+                .isInstanceOf(MyException.class);
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        RollbackService rollbackService() {
+            return new RollbackService();
+        }
+    }
     @Slf4j
     static class RollbackService {
+        // unchekced Exception -> 롤백
+        @Transactional
+        public void uncheckedException() {
+            log.info("call RuntimeException(unchecked)");
+            throw new RuntimeException();
+        }
+        // checked Exception -> 커밋
+        @Transactional(rollbackFor = MyException.class)
+        public void rollbackfor() throws MyException {
+            log.info("call rollbackfor");
+            throw new MyException();
+        }
+
+        @Transactional
+        public void checkedException() throws MyException {
+            log.info("call checkedException");
+            throw new MyException();
+        }
 
     }
 
-    // TODO 예외가 터지는데 왜 커밋하지? 가 중요!
+    static class MyException extends Exception {
+    }
+
 }
