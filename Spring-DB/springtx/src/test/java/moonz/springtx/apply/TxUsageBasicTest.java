2b3c42ee1587ef1fcd04623343fc5019ea707a60
BananMoon <younji1115@gmail.com>
2023-06-06 20:10:07 +0900

    style: 클래스명 수정

--- Spring-DB/springtx/src/test/java/moonz/springtx/apply/TxUsageBasicTest.java
+++ Spring-DB/springtx/src/test/java/moonz/springtx/apply/TxUsageBasicTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.*;
  */
 @Slf4j
 @SpringBootTest
-public class TxBasicTest {
+public class TxUsageBasicTest {
     @Autowired BasicService basicService;
 
     @Test
