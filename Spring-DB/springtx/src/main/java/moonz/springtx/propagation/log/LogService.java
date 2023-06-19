3801b6683f3fed9befb6c7edc9e73bc56e6a7d28
BananMoon <younji1115@gmail.com>
2023-06-19 23:56:10 +0900

    refactor: return 값 수정

--- Spring-DB/springtx/src/main/java/moonz/springtx/propagation/log/LogService.java
+++ Spring-DB/springtx/src/main/java/moonz/springtx/propagation/log/LogService.java
@@ -9,14 +9,13 @@ import org.springframework.stereotype.Service;
 public class LogService {
     public final LogRepository logRepository;
 
-    public Log save(String username) {
+    public void save(String username) {
         Log logMessage = new Log(username);
         log.info("== LogRepository 호출 시작 ==");
         if (username.contains("로그예외")) {
             throw new RuntimeException("예외 발생");
         }
-        Log savedLog = logRepository.save(logMessage);
+        logRepository.save(logMessage);
         log.info("== LogRepository 호출 종료 ==");
-        return savedLog;
     }
 }
