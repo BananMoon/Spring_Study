0db1f841b46cdc242e454693c3350b01bd25b4a6
BananMoon <younji1115@gmail.com>
2024-08-15 23:32:40 +0900

    feat: AuthenticationEntryPoint, AccessDeniedHandler 커스텀하여 등록한다.

--- Security-Practice/src/main/java/com/moonz/securitypractice/config/WebSecurityConfig.java
+++ Security-Practice/src/main/java/com/moonz/securitypractice/config/WebSecurityConfig.java
@@ -1,13 +1,16 @@
 package com.moonz.securitypractice.config;
 
+import com.moonz.securitypractice.security.CustomAccessDeniedHandler;
+import com.moonz.securitypractice.security.CustomAuthenticationEntryPoint;
 import com.moonz.securitypractice.security.jwt.JwtAuthenticationFilter;
-import com.moonz.securitypractice.security.jwt.JwtTokenProvider;
+import com.moonz.securitypractice.security.jwt.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,9 +22,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity  // Spring Security를 활성화해서 기본 스프링 필터체인에 등록시킴. SpringSecurityFilterChain을 자동 상속
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-    private final JwtTokenProvider jwtTokenProvider;
-//    private final CustomAuthenticationProvider authenticationProvider;
-
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final UserDetailsService userDetailsService;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -31,26 +35,27 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
-//                .authenticationProvider(authenticationProvider)
-//                .userDetailsService()
+                .authenticationProvider(jwtAuthenticationProvider)
+                .userDetailsService(userDetailsService)
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
-
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+                .and()
                 .authorizeRequests(authz -> authz
-                                .antMatchers("api/v1/user/login").authenticated()
-                                .antMatchers("api/v1/user/reissue").authenticated()
-                                .antMatchers("api/v1/user/signup").permitAll()
-                                .antMatchers("인증필요한요청api").hasAnyRole("USER", "ADMIN")
+                                .antMatchers("api/v1/login/**").authenticated()
+                                .antMatchers("api/v1/reissue").hasRole("USER")
+                                .antMatchers("api/v1/signup").permitAll()
+                                .antMatchers("api/v1/logout/**").hasAnyRole("USER", "ADMIN")
 //                        .access("hasRole('ROLE_USER')") // 특정 권한을 가지는 사용자만 접근 o
                                 .antMatchers("/h2-console/**").permitAll()
-//                                .antMatchers("/api/v1/user/signup").permitAll()
-                                .anyRequest().permitAll()   // 나머지 요청은 모두 가능
-                .and()
-                                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                )
+                                .anyRequest().permitAll()
+                        .and()
+                        .addFilterBefore(new JwtAuthenticationFilter(jwtAuthenticationProvider), UsernamePasswordAuthenticationFilter.class))
                 .build();
     }
 
--- Security-Practice/src/main/java/com/moonz/securitypractice/security/CustomAccessDeniedHandler.java
+++ Security-Practice/src/main/java/com/moonz/securitypractice/security/CustomAccessDeniedHandler.java
@@ -1,29 +1,28 @@
 package com.moonz.securitypractice.security;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moonz.securitypractice.exception.ErrorCode;
 import com.moonz.securitypractice.exception.dto.ErrorResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 커스텀하게 Security Filter에서 발생하는 인증 예외를 핸들링하는 클래스.
- * 인증 실패 시 해당 클래스에서 HTTP 응답을 처리한다.
+ * 커스텀하게 Security Filter에서 발생하는 인가 예외를 핸들링하는 클래스.
+ * 인가 실패 시 해당 클래스에서 HTTP 응답을 처리한다.
  */
 @RequiredArgsConstructor
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     private final ObjectMapper objectMapper;
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        ErrorResponseDto responseDto = ErrorResponseDto.of(ErrorCode.AUTH_REQUIRED);
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        ErrorResponseDto responseDto = ErrorResponseDto.of(ErrorCode.HANDLE_ACCESS_DENIED);
 
         String result = objectMapper.writeValueAsString(responseDto);
 
