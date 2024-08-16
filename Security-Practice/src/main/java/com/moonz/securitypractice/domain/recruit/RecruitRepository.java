de5cc755ee282c4434866e07b494822f36fb30ac
BananMoon <younji1115@gmail.com>
2024-08-16 23:34:07 +0900

    feat: 중복 클래스, 필요없는 클래스 제거한다.

--- Security-Practice/src/main/java/com/moonz/securitypractice/domain/recruit/RecruitRepository.java
+++
@@ -1,6 +0,0 @@
-package com.moonz.securitypractice.domain.recruit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-}
